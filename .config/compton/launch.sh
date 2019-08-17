5f408f0da39d5551e19231d14fb644f2e35774f5
Taxo Rubio <taxorubio@gmail.com>
2019-08-17 12:50:43 +0200

    Removed rogue .config directory (yikes!)

--- .config/compton/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ `ps -A | grep compton` ]]; then
-	killall -q compton
-	while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
-fi
-
-compton --config $HOME/.config/compton/config &
