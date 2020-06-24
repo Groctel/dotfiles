0e3ae506952f6daf6008337e9e22d7b47fa3e22d
Groctel <groctel@protonmail.com>
2020-06-24 10:33:53 +0200

    Removed menu and style tweaks

--- files/.config/rofi/drun.sh
+++ files/.config/rofi/drun.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# Original author's credits:
-
-# Author : Aditya Shakya (adi1090x)
-# Mail   : adi1090x@gmail.com
-# Github : @adi1090x
-# Reddit : @adi1090x
-
-rofi -no-lazy-grab -show drun -theme "row_dock".rasi
+rofi -no-lazy-grab -show drun -theme ~/.config/rofi/blurry_full.rasi
