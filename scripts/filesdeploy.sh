c990b48584fbbda4105ac27b0348decf89fae228
Groctel <groctel@protonmail.com>
2020-08-22 16:42:00 +0200

    Changed ifs to && statements

--- scripts/filesdeploy.sh
+++ scripts/filesdeploy.sh
@@ -9,40 +9,37 @@
 # ==============================================================================
 
 filesdeploy () {
-	overwrite=0
+	overwrite="false"
 
-	if confirm "yes" "Copy dotfiles fom repository to system"; then
-	#	Confirm overwriting
-		if confirm "no" "Overwrite existing files in system"; then
-			overwrite=1;
-		fi
-	#	Deploy dotfiles line by line
+	confirm "yes" "Copy dotfiles fom repository to system" && {
+		confirm "no" "Overwrite existing files in system" && overwrite="true"
+
 		while read -r line; do
 			printf "\033[1;35mDeploying \033[0m%s...\n" "$line";
+
 			directory="$(echo "$line" | sed 's/'"$(basename "$line")"'//')"
 			base="$(basename "$line")"
 
-			if [ $overwrite = 0 ]; then
+			[ "$overwrite" = "true" ] && {
 				if [ "$directory" != "" ] && [ -e ~/"$directory" ]; then
 					directory="$(echo "$directory" | sed 's/\/$/_\//')";
 				elif [ -e ~/"$line" ]; then
 					base="$(echo "$base" | sed 's/$/_/')";
 				fi
-			fi
+			}
 
 			mkdir -p ~/"$directory"
 
 			cp files/"$line" ~/"$directory$base"
 		done < filelist
-	fi
+	}
 
-#	Dependencies deploy
-	if confirm  "Install dotfiles' missing dependencies"; then
-	#	Antigen (zsh plugin manager)
-		curl -L git.io/antigen > ~/.antigen.zsh;
-	#	Plug (vim plugin manager)
+# Dependencies deploy
+	confirm "Install dotfiles' missing dependencies" && {
+		curl -fLo ~/.antigen.zsh git.io/antigen
 		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 		     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 		vim +"so ~/.vimrc" +PlugInstall +qall
-	fi
+	}
 }
