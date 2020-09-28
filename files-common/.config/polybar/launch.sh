41af8397cecdf65ed1daafd8a23de73746b9d3f7
Groctel <groctel@protonmail.com>
2020-09-28 07:45:54 +0200

    Modularised and simplified polybar files

--- files-common/.config/polybar/launch.sh
+++ files-common/.config/polybar/launch.sh
@@ -1,15 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/sh
 
-## Add this to your wm startup file.
-
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	# Launch bar1 and bar2
-	MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1)
+do
+	export MONITOR="$m"
+	polybar -c ~/.config/polybar/config.ini main &
 done
